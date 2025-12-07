ec46afec37d2f51e1f7e8f8177e263f31b2bb4ac
zosmac <zosmac@github.com>
2025-12-07 13:35:44 -0500

    push initial project

---
+++ GomonChart/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GomonChart
+//
+//  Created by Keefe Hayes on 12/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GomonChart/GomonChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  GomonChartApp.swift
+//  GomonChart
+//
+//  Created by Keefe Hayes on 12/7/25.
+//
+
+import SwiftUI
+
+@main
+struct GomonChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
